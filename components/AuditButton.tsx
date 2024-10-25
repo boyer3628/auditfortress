c3c5c9b2ab8824e2cb9bfbdcea9c833a4c2e1dc2
Jason Boyer <jason.boyer@amentum.com>
2024-10-25 13:51:42 -0500

    fixed button images

--- components/AuditButton.tsx
+++ components/AuditButton.tsx
@@ -20,8 +20,8 @@ export function AuditButton({ type, label, subtitle, href, imagePath }: AuditBut
           <Image
             src={imagePath}
             alt={`${type} audit icon`}
-            width={200}
-            height={200}
+            fill
+            style={{ objectFit: "cover" }}
             priority
           />
         </div>
